67765ce6bcab5c3f8e89414ff648d191b3b5f18a
jihaifeng <1104128141@qq.com>
2017-07-18 14:02:01 +0800

    up

--- app/src/main/java/com/alarmclocksnoozers/runnershigh/Menu.java
+++ app/src/main/java/com/alarmclocksnoozers/runnershigh/Menu.java
@@ -17,41 +17,39 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 public class Menu extends Activity {
-	MediaPlayer menuLoop;
-	private Toast loadMessage;
-	private Runnable gameLauncher;
-	private Intent gameIntent;
-	private Handler mHandler;
-	private android.widget.Button mPlayButton;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+  MediaPlayer menuLoop;
+  private Toast loadMessage;
+  private Runnable gameLauncher;
+  private Intent gameIntent;
+  private Handler mHandler;
+  private android.widget.Button mPlayButton;
 
-    	requestWindowFeature(Window.FEATURE_NO_TITLE);  
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-    	super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu); 
-        
+  @Override public void onCreate(Bundle savedInstanceState) {
 
-		loadMessage = Toast.makeText(getApplicationContext(), "loading game...", Toast.LENGTH_SHORT );
-		loadMessage.setGravity(Gravity.CENTER|Gravity.CENTER, 0, 0);
-        
-		gameIntent = new Intent (this, main.class);
-		mPlayButton = (android.widget.Button)findViewById(R.id.startButton);
-		mPlayButton.setClickable(true);
-		mPlayButton.setEnabled(true);
-		gameLauncher = new Runnable() {
-			
-			public void run() {
-				mPlayButton.setClickable(false);
-		    	mPlayButton.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
-				startActivityForResult(gameIntent, 0);
-			}
-		};
-		
-		mHandler = new Handler();
-		
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.menu);
+
+    loadMessage = Toast.makeText(getApplicationContext(), "loading game...", Toast.LENGTH_SHORT);
+    loadMessage.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+
+    gameIntent = new Intent(this, main.class);
+    mPlayButton = (android.widget.Button) findViewById(R.id.startButton);
+    mPlayButton.setClickable(true);
+    mPlayButton.setEnabled(true);
+    gameLauncher = new Runnable() {
+
+      public void run() {
+        mPlayButton.setClickable(false);
+        mPlayButton.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY);
+        startActivityForResult(gameIntent, 0);
+      }
+    };
+
+    mHandler = new Handler();
+
 		
 		
         /*
@@ -61,61 +59,59 @@ public class Menu extends Activity {
         menuLoop.setVolume(0.5f, 0.5f);
         menuLoop.start();
         */
-    }
-    
-    public void playGame(View view) {
+  }
 
-		// Loading Toast
-		loadMessage.show();
-    	Settings.SHOW_FPS = false;
-    	mHandler.post(gameLauncher);
-    }
-    
-    public void playGameWithFPS(View view) {
+  public void playGame(View view) {
 
-		// Loading Toast
-		loadMessage.show();
-    	Settings.SHOW_FPS = true;
-    	mHandler.post(gameLauncher);
+    // Loading Toast
+    loadMessage.show();
+    Settings.SHOW_FPS = false;
+    mHandler.post(gameLauncher);
+  }
+
+  public void playGameWithFPS(View view) {
+
+    // Loading Toast
+    loadMessage.show();
+    Settings.SHOW_FPS = true;
+    mHandler.post(gameLauncher);
+  }
+
+  public void showScore(View view) {
+    Intent myIntent = new Intent(this, HighScoreActivity.class);
+    startActivity(myIntent);
+  }
+
+  public void showInfo(View view) {
+    Intent myIntent = new Intent(this, Info.class);
+    startActivity(myIntent);
+  }
+
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode == 1) {
+      showDialog(1);
+      mHandler.postDelayed(new Runnable() {
+
+        public void run() {
+          mPlayButton.setClickable(true);
+          mPlayButton.getBackground().clearColorFilter();
+        }
+      }, 10000);
+    } else {
+      mPlayButton.setClickable(true);
+      mPlayButton.getBackground().clearColorFilter();
     }
-    
-    public void showScore(View view) {
-    	Intent myIntent = new Intent (this, HighScoreActivity.class);
-    	startActivity (myIntent);
-    }
-    
-    public void showInfo(View view) {
-    	Intent myIntent = new Intent (this, Info.class);
-    	startActivity (myIntent);
-    }
-    
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-    	if (resultCode == 1) {
-    		showDialog(1);
-    		mHandler.postDelayed(new Runnable() {
-				
-				public void run() {
-					mPlayButton.setClickable(true);
-					mPlayButton.getBackground().clearColorFilter();
-				}
-			}, 10000);
-    	} else {
-    		mPlayButton.setClickable(true);
-    		mPlayButton.getBackground().clearColorFilter();
-    	}
-    	
-    }
-    
-    public void donate(View view) {
-    	Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse(Settings.URL_DONATE));
-    	startActivity(browserIntent);
-    }
-    
-    protected Dialog onCreateDialog(int id) {
-    	return new AlertDialog.Builder(this)
-		  .setTitle("Error while changing view")
-		  .setMessage("System needs some time to free memory. Please try again in 10 seconds.")
-		  .setCancelable(true)
-		  .create();
-    }
+  }
+
+  public void donate(View view) {
+    Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse(Settings.URL_DONATE));
+    startActivity(browserIntent);
+  }
+
+  protected Dialog onCreateDialog(int id) {
+    return new AlertDialog.Builder(this).setTitle("Error while changing view")
+        .setMessage("System needs some time to free memory. Please try again in 10 seconds.")
+        .setCancelable(true)
+        .create();
+  }
 }
